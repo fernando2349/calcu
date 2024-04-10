ef9f59ddc587388a03a515856b5bceb7f25299ee
fernando2349 <146887271+fernando2349@users.noreply.github.com>
2024-04-10 16:51:18 -0500

    main
    inicio calculadora

---
+++ 01-Lab-react-native-with-cli-calculator/src/App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar, Text, View } from "react-native";
+
+function App() {
+  return(
+    <View>
+      <StatusBar barStyle={'light-content'} backgroundColor={'black'} />
+
+      <Text>Calculadora</Text>
+    </View>
+  )
+}
+
+export default App
